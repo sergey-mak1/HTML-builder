94ca0fefe9a5ae3b1bdde47139932f4b64118a55
sergey-mak1 <sega013021990@gmail.com>
2023-05-02 21:15:11 +0300

    feat: add solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path')
+const notes = './01-read-file/text.txt'
+const stream = new fs.ReadStream(path.normalize(notes),'utf-8');
+stream.on('readable', function(){
+    const data = stream.read();
+    if(data!==null){
+        console.log(data);
+    }
+    
+});
