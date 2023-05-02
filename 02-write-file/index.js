9cc4946fb5ddc5560be8992015e6bbf2900ac98a
sergey-mak1 <sega013021990@gmail.com>
2023-05-02 22:27:55 +0300

    feat: add solution 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const process = require('node:process');
+const fs = require('fs');
+const path = require('path');
+const file = fs.createWriteStream(path.join(__dirname, 'file.txt'));
+process.stdout.write('Привет, напиши пару строк: ');
+process.stdin.on('data', (text) =>{
+    if(text.toString().trim()!=='exit'){
+        file.write(text)
+    }
+    else{
+        process.exit();
+    }
+})
+process.on('SIGINT',process.exit);
+process.on('exit',()=>process.stdout.write('GOOD BYE!'))
+
